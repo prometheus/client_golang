4cab4a892e3f5958279709f2e92629038d420dc6
Matt T. Proud <matt.proud@gmail.com>
2013-01-18 15:47:40 +0100

    Include instance uptime information in the stack.

---
+++ constants.go
@@ -0,0 +1,14 @@
+/*
+Copyright (c) 2013, Matt T. Proud
+All rights reserved.
+
+Use of this source code is governed by a BSD-style license that can be found in
+the LICENSE file.
+*/
+
+package registry
+
+var (
+	// NilLabels is a nil set of labels merely for end-user convenience.
+	NilLabels map[string]string
+)
