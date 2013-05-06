af56a93a8072bd57ea04d5024ed5a249949b4755
Bernerd Schaefer <bj.schaefer@gmail.com>
2013-05-06 11:13:44 +0200

    Rename test helper files to helpers_test.go
    This ensures that these files are properly included only in testing.
    
    [Fixes #10]

--- prometheus/helpers_test.go
+++ prometheus/helpers_test.go
@@ -38,3 +38,18 @@ func isNaN(obtained interface{}) (result bool) {
 
 	return false
 }
+
+type valueEqualsChecker struct {
+	*CheckerInfo
+}
+
+var ValueEquals Checker = &valueEqualsChecker{
+	&CheckerInfo{Name: "IsValue", Params: []string{"obtained", "expected"}},
+}
+
+func (checker *valueEqualsChecker) Check(params []interface{}, names []string) (result bool, error string) {
+	actual := params[0].(*item).Value
+	expected := params[1]
+
+	return actual == expected, ""
+}
