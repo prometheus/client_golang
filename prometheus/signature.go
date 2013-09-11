26ad852c94e7ce0df78d03767f09b8c8ccafad29
Bernerd Schaefer <bj.schaefer@gmail.com>
2013-09-11 17:16:34 +0200

    Cache signature of an empty label set
    This is an optimization of labelsToSignature to avoid excess allocations
    when the label set is empty.
    
    Change-Id: If2d59bbc3ae6d4457e2ded197b6f4e7c67e6a173

--- prometheus/signature.go
+++ prometheus/signature.go
@@ -14,9 +14,16 @@ import (
 	"github.com/prometheus/client_golang/model"
 )
 
+// cache the signature of an empty label set.
+var emptyLabelSignature = fnv.New64a().Sum64()
+
 // LabelsToSignature provides a way of building a unique signature
 // (i.e., fingerprint) for a given label set sequence.
 func labelsToSignature(labels map[string]string) uint64 {
+	if len(labels) == 0 {
+		return emptyLabelSignature
+	}
+
 	names := make(model.LabelNames, 0, len(labels))
 	for name := range labels {
 		names = append(names, model.LabelName(name))
