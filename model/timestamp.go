6c83eeccca6e5b8aa56b07e438ba6213d9f61f3e
Julius Volz <julius@soundcloud.com>
2013-10-29 16:36:58 +0100

    Add custom Timestamp/Duration types for sample times.
    So far we've been using Go's native time.Time for anything related to sample
    timestamps. Since the range of time.Time is much bigger than what we need, this
    has created two problems:
    
    - there could be time.Time values which were out of the range/precision of the
      time type that we persist to disk, therefore causing incorrectly ordered keys.
      One bug caused by this was:
    
      https://github.com/prometheus/prometheus/issues/367
    
      It would be good to use a timestamp type that's more closely aligned with
      what the underlying storage supports.
    
    - sizeof(time.Time) is 192, while Prometheus should be ok with a single 64-bit
      Unix timestamp (possibly even a 32-bit one). Since we store samples in large
      numbers, this seriously affects memory usage. Furthermore, copying/working
      with the data will be faster if it's smaller.
    
    *MEMORY USAGE RESULTS*
    Initial memory usage comparisons for a running Prometheus with 1 timeseries and
    100,000 samples show roughly a 13% decrease in total (VIRT) memory usage. In my
    tests, this advantage for some reason decreased a bit the more samples the
    timeseries had (to 5-7% for millions of samples). This I can't fully explain,
    but perhaps garbage collection issues were involved.
    
    *WHEN TO USE THE NEW TIMESTAMP TYPE*
    The new clientmodel.Timestamp type and its related clientmodel.Duration type
    should be used whenever time calculations are either directly or indirectly
    related to sample timestamps.
    
    For example:
    - the timestamp of a sample itself
    - all kinds of watermarks
    - anything that may become or is compared to a sample timestamp (like the timestamp
      passed into Target.Scrape()).
    
    When to still use time.Time:
    - for measuring durations/times not related to sample timestamps, like duration
      telemetry exporting, timers that indicate how frequently to execute some
      action, etc.
    
    Change-Id: I253a467388774280c10400fda122369ff77c1730

---
+++ model/timestamp.go
@@ -0,0 +1,103 @@
+// Copyright 2013 Prometheus Team
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import (
+	"fmt"
+	native_time "time"
+)
+
+// TODO(julius): Should this use milliseconds/nanoseconds instead? This is
+//               mostly hidden from the user of these types when using the
+//               methods below, so it will be easy to change this later
+//               without requiring significant user code changes.
+
+// Time in seconds since the epoch (January 1, 1970 UTC).
+type Timestamp int64
+
+// Duration in seconds.
+type Duration int64
+
+const (
+	// Second must be less or equal to the value of time.Second!
+	Second Duration = 1
+	Minute          = 60 * Second
+	Hour            = 60 * Minute
+)
+
+// Equal reports whether two timestamps represent the same instant.
+func (t Timestamp) Equal(o Timestamp) bool {
+	return t == o
+}
+
+// Before reports whether the timestamp t is before o.
+func (t Timestamp) Before(o Timestamp) bool {
+	return t < o
+}
+
+// Before reports whether the timestamp t is after o.
+func (t Timestamp) After(o Timestamp) bool {
+	return t > o
+}
+
+// Add returns the Timestamp t + d.
+func (t Timestamp) Add(d Duration) Timestamp {
+	return t + Timestamp(d)
+}
+
+// Sub returns the Duration t - o.
+func (t Timestamp) Sub(o Timestamp) Duration {
+	return Duration(t - o)
+}
+
+// Time returns the time.Time representation of t.
+func (t Timestamp) Time() native_time.Time {
+	return native_time.Unix(int64(t)/int64(Second), (int64(t) % int64(Second)))
+}
+
+// Unix returns t as a Unix time, the number of seconds elapsed
+// since January 1, 1970 UTC.
+func (t Timestamp) Unix() int64 {
+	return int64(t) / int64(Second)
+}
+
+// String returns a string representation of the timestamp.
+func (t Timestamp) String() string {
+	return fmt.Sprintf("%d", t)
+}
+
+// Now returns the current time as a Timestamp.
+func Now() Timestamp {
+	return TimestampFromTime(native_time.Now())
+}
+
+// TimestampFromTime returns the Timestamp equivalent to the time.Time t.
+func TimestampFromTime(t native_time.Time) Timestamp {
+	return TimestampFromUnix(t.Unix())
+}
+
+// TimestampFromUnix returns the Timestamp equivalent to the Unix timestamp t.
+func TimestampFromUnix(t int64) Timestamp {
+	return Timestamp(t * int64(Second))
+}
+
+// TimeDuration returns the time.Duration equivalent to d.
+func (d Duration) TimeDuration() native_time.Duration {
+	return native_time.Duration(d) * (native_time.Second / native_time.Duration(Second))
+}
+
+// NewDuration creates a new Duration equivalent to the time.Duration t.
+func NewDuration(t native_time.Duration) Duration {
+	return Duration(t / (native_time.Second / native_time.Duration(Second)))
+}
