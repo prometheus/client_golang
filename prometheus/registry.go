9a932a61d673661afd561f71c912d1554e45b885
Matt T. Proud <matt.proud@gmail.com>
2014-04-18 11:57:57 +0200

    Interim checkpoint.
    Change-Id: Ia69da3b8d0394dd0982c532fcab3af27c05e1a4d

--- prometheus/registry.go
+++ prometheus/registry.go
@@ -1,357 +1,185 @@
-// Copyright (c) 2013, Prometheus Team
+// Copyright (c) 2014, Prometheus Team
 // All rights reserved.
 //
-// Use of this source code is governed by a BSD-style license that can be found
-// in the LICENSE file.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package prometheus
 
 import (
+	"errors"
 	"compress/gzip"
-	"encoding/base64"
-	"encoding/json"
-	"flag"
-	"fmt"
-	"io"
-	"log"
+	"bytes"
 	"net/http"
-	"sort"
-	"strings"
 	"sync"
-	"time"
 
-	dto "github.com/prometheus/client_model/go"
-
-	"code.google.com/p/goprotobuf/proto"
-	"github.com/matttproud/golang_protobuf_extensions/ext"
-
-	"github.com/prometheus/client_golang/model"
 	"github.com/prometheus/client_golang/vendor/goautoneg"
 )
 
-const (
-	authorization            = "Authorization"
-	authorizationHeader      = "WWW-Authenticate"
-	authorizationHeaderValue = "Basic"
+var errAlreadyReg = errors.New("duplicate metric registration attempted")
 
-	acceptEncodingHeader     = "Accept-Encoding"
-	contentEncodingHeader    = "Content-Encoding"
-	contentTypeHeader        = "Content-Type"
-	gzipAcceptEncodingValue  = "gzip"
-	gzipContentEncodingValue = "gzip"
-	jsonContentType          = "application/json"
-)
-
-// container represents a top-level registered metric that encompasses its
-// static metadata.
-type container struct {
-	BaseLabels map[string]string `json:"baseLabels"`
-	Docstring  string            `json:"docstring"`
-	Metric     Metric            `json:"metric"`
-	name       string
-}
-
-type containers []*container
-
-func (c containers) Len() int {
-	return len(c)
-}
-
-func (c containers) Swap(i, j int) {
-	c[i], c[j] = c[j], c[i]
-}
-
-func (c containers) Less(i, j int) bool {
-	return c[i].name < c[j].name
-}
+const numBufs = 4
 
 type registry struct {
-	mutex                     sync.RWMutex
-	signatureContainers       map[uint64]*container
-	metricFamilyInjectionHook func() []*dto.MetricFamily
+	mtx     sync.RWMutex
+	metrics map[string]Metric
+
+	bufs chan *bytes.Buffer
 }
 
-// Registry is a registrar where metrics are listed.
-//
-// In most situations, using DefaultRegistry is sufficient versus creating one's
-// own.
-type Registry interface {
-	// Register a metric with a given name.  Name should be globally unique.
-	Register(name, docstring string, baseLabels map[string]string, metric Metric) error
-	// SetMetricFamilyInjectionHook sets a function that is called whenever
-	// metrics are requested. The MetricsFamily protobufs returned by the
-	// function are appended to the delivered metrics. This is a way to
-	// directly inject MetricFamily protobufs managed and owned by the
-	// caller. The caller has full responsibility. No sanity checks are
-	// performed on the returned protobufs. The function must be callable at
-	// any time and concurrently. The only thing handled by the Registry is
-	// the conversion if metrics are requested in a non-protobuf format. The
-	// deprecated JSON format, however, is not supported, i.e. metrics
-	// delivered as JSON will not contain the metrics injected by the
-	// injection hook.
-	SetMetricFamilyInjectionHook(func() []*dto.MetricFamily)
-	// Handler creates a http.HandlerFunc. Requests against it generate a
-	// representation of the metrics managed by this registry.
-	Handler() http.HandlerFunc
-	// YieldExporter is a legacy version of Handler and is deprecated.
-	// Please stop using.
-	YieldExporter() http.HandlerFunc
-}
-
-// This builds a new metric registry.  It is not needed in the majority of
-// cases.
-func NewRegistry() Registry {
-	return &registry{
-		signatureContainers: make(map[uint64]*container),
-	}
-}
-
-// Associate a Metric with the DefaultRegistry.
-func Register(name, docstring string, baseLabels map[string]string, metric Metric) error {
-	return DefaultRegistry.Register(name, docstring, baseLabels, metric)
-}
-
-// SetMetricFamilyInjectionHook implements the Registry interface.
-func (r *registry) SetMetricFamilyInjectionHook(hook func() []*dto.MetricFamily) {
-	r.metricFamilyInjectionHook = hook
-}
-
-// Implements json.Marshaler
-func (r *registry) MarshalJSON() ([]byte, error) {
-	containers := make(containers, 0, len(r.signatureContainers))
-
-	for _, container := range r.signatureContainers {
-		containers = append(containers, container)
-	}
-
-	sort.Sort(containers)
-
-	return json.Marshal(containers)
-}
-
-// isValidCandidate returns true if the candidate is acceptable for use.  In the
-// event of any apparent incorrect use it will report the problem, invalidate
-// the candidate, or outright abort.
-func (r *registry) isValidCandidate(name string, baseLabels map[string]string) (signature uint64, err error) {
-	if len(name) == 0 {
-		err = fmt.Errorf("unnamed metric named with baseLabels %s is invalid", baseLabels)
-
-		if *abortOnMisuse {
-			panic(err)
-		} else if *debugRegistration {
-			log.Println(err)
-		}
-	}
-
-	for label := range baseLabels {
-		if strings.HasPrefix(label, model.ReservedLabelPrefix) {
-			err = fmt.Errorf("metric named %s with baseLabels %s contains reserved label name %s in baseLabels", name, baseLabels, label)
-
-			if *abortOnMisuse {
-				panic(err)
-			} else if *debugRegistration {
-				log.Println(err)
-			}
-
-			return signature, err
-		}
-	}
-
-	baseLabels[string(model.MetricNameLabel)] = name
-	signature = labelsToSignature(baseLabels)
-
-	if _, contains := r.signatureContainers[signature]; contains {
-		err = fmt.Errorf("metric named %s with baseLabels %s is already registered", name, baseLabels)
-		if *abortOnMisuse {
-			panic(err)
-		} else if *debugRegistration {
-			log.Println(err)
-		}
-
-		return signature, err
-	}
-
-	if *useAggressiveSanityChecks {
-		for _, container := range r.signatureContainers {
-			if container.name == name {
-				err = fmt.Errorf("metric named %s with baseLabels %s is already registered as %s and risks causing confusion", name, baseLabels, container.BaseLabels)
-				if *abortOnMisuse {
-					panic(err)
-				} else if *debugRegistration {
-					log.Println(err)
-				}
-
-				return signature, err
-			}
-		}
-	}
-
-	return signature, err
-}
-
-func (r *registry) Register(name, docstring string, baseLabels map[string]string, metric Metric) error {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-
-	labels := map[string]string{}
-
-	if baseLabels != nil {
-		for k, v := range baseLabels {
-			labels[k] = v
-		}
-	}
-
-	signature, err := r.isValidCandidate(name, labels)
-	if err != nil {
+func (r *registry) Register(m Metric) (Metric, error) {
+	desc := m.Desc()
+	if err := desc.build(); err != nil {
 		return err
 	}
 
-	r.signatureContainers[signature] = &container{
-		BaseLabels: labels,
-		Docstring:  docstring,
-		Metric:     metric,
-		name:       name,
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	if existing, exists := r.metrics[desc.canonName]; exists {
+		return existing, errAlreadyReg
 	}
 
-	return nil
+	r.metrics[desc.canonName] = m
+
+	return m, nil
 }
 
-// YieldBasicAuthExporter creates a http.HandlerFunc that is protected by HTTP's
-// basic authentication.
-func (register *registry) YieldBasicAuthExporter(username, password string) http.HandlerFunc {
-	// XXX: Work with Daniel to get this removed from the library, as it is really
-	//      superfluous and can be much more elegantly accomplished via
-	//      delegation.
-	log.Println("Registry.YieldBasicAuthExporter is deprecated.")
-	exporter := register.YieldExporter()
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		authenticated := false
-
-		if auth := r.Header.Get(authorization); auth != "" {
-			base64Encoded := strings.SplitAfter(auth, " ")[1]
-			decoded, err := base64.URLEncoding.DecodeString(base64Encoded)
-			if err == nil {
-				usernamePassword := strings.Split(string(decoded), ":")
-				if usernamePassword[0] == username && usernamePassword[1] == password {
-					authenticated = true
-				}
-			}
-		}
-
-		if authenticated {
-			exporter.ServeHTTP(w, r)
-		} else {
-			w.Header().Add(authorizationHeader, authorizationHeaderValue)
-			http.Error(w, "access forbidden", 401)
-		}
-	})
-}
-
-// decorateWriter annotates the response writer to handle any other behaviors
-// that might be beneficial to the client---e.g., GZIP encoding.
-func decorateWriter(request *http.Request, writer http.ResponseWriter) io.Writer {
-	if !strings.Contains(request.Header.Get(acceptEncodingHeader), gzipAcceptEncodingValue) {
-		return writer
+func (r *registry) RegisterOrGet(m Metric) error {
+	existing, err := r.Register(m)
+	if err != nil && err != errAlreadyReg {
+		return nil, err
 	}
 
-	writer.Header().Set(contentEncodingHeader, gzipContentEncodingValue)
-	gziper := gzip.NewWriter(writer)
-
-	return gziper
+	return existing, nil
 }
 
-func (registry *registry) YieldExporter() http.HandlerFunc {
-	log.Println("Registry.YieldExporter is deprecated in favor of Registry.Handler.")
+func (r *registry) Unregister(m Metric) (bool, error) {
+	desc := m.Desc()
+	if err := desc.build(); err != nil {
+		return false, err
+	}
 
-	return registry.Handler()
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	if _, ok := r.metrics[desc.canonName]; !ok {
+		return false, nil
+	}
+	del(r.metrics, desc.canonName)
+
+	return true, nil
 }
 
-func (r *registry) dumpDelimitedPB(w io.Writer) {
-	r.mutex.RLock()
-	defer r.mutex.RUnlock()
+func (r *registry) getBuf() *bytes.Buffer {
+	select {
+	case buf := <- r.bufs:
+		return buf
+	default:
+		return &bytes.Buffer{}
+	}
+}
 
-	f := new(dto.MetricFamily)
-	for _, container := range r.signatureContainers {
-		f.Reset()
+func (r *registry) giveBuf(buf * bytes.Buffer) {
+	select {
+	case r.bufs <- buf:
+		buf.Reset()
+	default:
+	}
+}
 
-		f.Name = proto.String(container.name)
-		f.Help = proto.String(container.Docstring)
+func newRegistry() *registry {
+	return &registry{
+		metrics: make(map[string]Metric),
+		bufs: make(chan *byte.Buffer, numBufs),
+	}
+}
 
-		container.Metric.dumpChildren(f)
+var defRegistry = newRegistry()
 
-		for name, value := range container.BaseLabels {
-			if model.LabelName(name) == model.MetricNameLabel {
-				// The name is already in MetricFamily.
+// Handler is the default Prometheus http.HandlerFunc for the global metric
+// registry.
+var Handler =  defRegistry.handle
+
+// MustRegister enrolls a new metric.  It panics if the provided Desc is
+// problematic or the metric is already registered.  It returns the enrolled
+// metric.
+func MustRegister(m Metric) Metric {
+	 m, err := defRegistry.Register(m)
+	if err != nil {
+		panic(err)
+	}
+	return m
+}
+
+// MustRegisterOrGet enrolls a new metric once and only once.  It panics if the
+// provided Desc is problematic.  It returns the enrolled metric or the existing
+// one.
+func MustRegisterOrGet(m Metric) Metric {
+	existing, err := defRegistry.RegisterOrGet(m)
+	if err != nil {
+		panic(err)
+	}
+	return existing
+}
+
+// Unregister unenrolls a metric returning whether the metric was unenrolled and
+// whether an error existed.
+func Unregister(m Metric) (bool, error) {
+	return defRegistry.Unregister(m)
+}
+
+func chooseType(req *http.Request) func(io.Writer) error {
+	accepts := goautoneg.ParseAccept(r.Header.Get("Accept"))
+	for _, accept := range accepts {
+		if accept.Type != "application" {
+ 			continue
+		}
+
+		if accept.SubType == "vnd.google.protobuf" {
+ 			if accept.Params["proto"] != "io.prometheus.client.MetricFamily" {
 				continue
-				// TODO: Once JSON is history, do not anymore
-				// add the __name__ label to BaseLabels and
-				// then remove this check.
 			}
-			p := &dto.LabelPair{
-				Name:  proto.String(name),
-				Value: proto.String(value),
+			if accept.Params["encoding"] != "delimited" {
+				continue
 			}
 
-			for _, child := range f.Metric {
-				child.Label = append(child.Label, p)
-			}
+			return outputProto
 		}
-
-		ext.WriteDelimited(w, f)
 	}
+
+	return outputText
 }
 
-func (r *registry) dumpDelimitedExternalPB(w io.Writer) {
-	if r.metricFamilyInjectionHook == nil {
+func outputProto(w io.Writer) error {
+	panic("unimpl")
+}
+
+func outputText(w io.Writer) error {
+	panic("unimpl")
+}
+
+func chooseAcceptEncoding(req *http.Request, w io.Writer) io.Writer {
+	if !strings.Contains(req.Header.Get("Accept-Encoding"), "gzip") {
+		return gzip.NewWriter(w)
+	}
+
+	return w
+}
+
+func (r *registry) handle(w http.ResponseWriter, req *http.Request) {
+	format := chooseType(req)
+	buf := r.getBuf()
+	defer r.giveBuf(buf)
+	writer := chooseAcceptEncoding(req, buf)
+	if closer, ok := writer.(io.Closer); ok {
+		defer closer.Close()
+	}
+	if err := format(writer); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
-	for _, f := range r.metricFamilyInjectionHook() {
-		ext.WriteDelimited(w, f)
-	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(buf.Bytes())
 }
-
-func (registry *registry) Handler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		defer requestLatencyAccumulator(time.Now())
-
-		requestCount.Increment(nil)
-		header := w.Header()
-
-		writer := decorateWriter(r, w)
-
-		if closer, ok := writer.(io.Closer); ok {
-			defer closer.Close()
-		}
-
-		accepts := goautoneg.ParseAccept(r.Header.Get("Accept"))
-		for _, accept := range accepts {
-			if accept.Type != "application" {
-				continue
-			}
-
-			if accept.SubType == "vnd.google.protobuf" {
-				if accept.Params["proto"] != "io.prometheus.client.MetricFamily" {
-					continue
-				}
-				if accept.Params["encoding"] != "delimited" {
-					continue
-				}
-
-				header.Set(contentTypeHeader, DelimitedTelemetryContentType)
-				registry.dumpDelimitedPB(writer)
-				registry.dumpDelimitedExternalPB(writer)
-				return
-			}
-		}
-
-		header.Set(contentTypeHeader, TelemetryContentType)
-		json.NewEncoder(writer).Encode(registry)
-	}
-}
-
-var (
-	abortOnMisuse             = flag.Bool(FlagNamespace+"abortonmisuse", false, "abort if a semantic misuse is encountered (bool).")
-	debugRegistration         = flag.Bool(FlagNamespace+"debugregistration", false, "display information about the metric registration process (bool).")
-	useAggressiveSanityChecks = flag.Bool(FlagNamespace+"useaggressivesanitychecks", false, "perform expensive validation of metrics (bool).")
-)
