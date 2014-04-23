84dc53148d6abf392427cb334e15cdac834d651a
Bjoern Rabenstein <bjoern@soundcloud.com>
2014-04-23 13:40:37 +0200

    Enable the Golang client library to create the new text formats.
    Most important here is the simple & flat text format, but while I'm on
    it, I have also added the text representations for protobufs (which is
    purely meant for debugging purposes). I hope my basic idea about
    handling those various protocols (and the text package) becomes
    clearer now.
    
    Change-Id: I7299853eadc82a426101e907f2b3d4e37f9e4c71

--- prometheus/constants.go
+++ prometheus/constants.go
@@ -26,14 +26,27 @@ const (
 	// APIVersion is the version of the format of the exported data.  This
 	// will match this library's version, which subscribes to the Semantic
 	// Versioning scheme.
-	APIVersion = "0.0.2"
+	APIVersion = "0.0.4"
 
-	// TelemetryContentType is the content type and schema information set
-	// on telemetry data responses.
-	TelemetryContentType = `application/json; schema="prometheus/telemetry"; version=` + APIVersion
-	// DelimitedTelemetryContentType is the content type and schema
-	// information set on telemetry data responses.
+	// JSONAPIVersion is the version of the JSON export format.
+	JSONAPIVersion = "0.0.2"
+
+	// DelimitedTelemetryContentType is the content type set on telemetry
+	// data responses in delimited protobuf format.
 	DelimitedTelemetryContentType = `application/vnd.google.protobuf; proto="io.prometheus.client.MetricFamily"; encoding="delimited"`
+	// TextTelemetryContentType is the content type set on telemetry data
+	// responses in text format.
+	TextTelemetryContentType = `text/plain; version=` + APIVersion
+	// ProtoTextTelemetryContentType is the content type set on telemetry
+	// data responses in protobuf text format.  (Only used for debugging.)
+	ProtoTextTelemetryContentType = `application/vnd.google.protobuf; proto="io.prometheus.client.MetricFamily"; encoding="text"`
+	// ProtoCompactTextTelemetryContentType is the content type set on
+	// telemetry data responses in protobuf compact text format.  (Only used
+	// for debugging.)
+	ProtoCompactTextTelemetryContentType = `application/vnd.google.protobuf; proto="io.prometheus.client.MetricFamily"; encoding="compact-text"`
+	// JSONTelemetryContentType is the content type set on telemetry data
+	// responses formatted as JSON.
+	JSONTelemetryContentType = `application/json; schema="prometheus/telemetry"; version=` + JSONAPIVersion
 
 	// ExpositionResource is the customary web services endpoint on which
 	// telemetric data is exposed.
