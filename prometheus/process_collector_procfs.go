21b7c6b0483ccfc3e9983af61fb049498c0f5fb7
Julius Volz <julius@soundcloud.com>
2015-03-31 18:07:40 +0200

    Always build the procfs collector.
    The cgo dependency in the "procfs" package is being removed in:
    
      https://github.com/prometheus/procfs/pull/4
    
    So now it should be fine to always import the "procfs" package and have
    it determine dynamically at runtime whether the proc filesystem is
    actually there.
    
    This fixes a problem for users which were vendoring client_golang on
    MacOS X, but building the result on Linux (the procfs package was
    missing in the vendored packages).

--- prometheus/process_collector_procfs.go
+++ prometheus/process_collector_procfs.go
@@ -11,8 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build linux,cgo plan9,cgo solaris,cgo
-
 package prometheus
 
 import "github.com/prometheus/procfs"
