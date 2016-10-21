8381520bfdc8d85af039e166fa29c0e7c554e96c
stuart nelson <stuartnelson3@gmail.com>
2016-10-21 12:27:47 +0200

    Add test for push

--- prometheus/graphite/bridge_test.go
+++ prometheus/graphite/bridge_test.go
@@ -1,6 +1,11 @@
 package graphite
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"regexp"
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -54,4 +59,98 @@ prefix.nameconstname.constvalue.labelname.val2 1 1477043083
 	if got := buf.String(); want != got {
 		t.Errorf("wanted \n%s\n, got \n%s\n", want, got)
 	}
+}
+
+func TestPush(t *testing.T) {
+	reg := prometheus.NewRegistry()
+	cntVec := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:        "name",
+			Help:        "docstring",
+			ConstLabels: prometheus.Labels{"constname": "constvalue"},
+		},
+		[]string{"labelname"},
+	)
+	cntVec.WithLabelValues("val1").Inc()
+	cntVec.WithLabelValues("val2").Inc()
+	reg.MustRegister(cntVec)
+
+	host := "localhost"
+	port := ":56789"
+	b, err := NewBridge(&Config{
+		URL:      host + port,
+		Gatherer: reg,
+		Prefix:   "prefix",
+	})
+	if err != nil {
+		t.Errorf("error creating bridge: %v", err)
+	}
+	defer b.Stop()
+
+	nmg, err := newMockGraphite(port)
+	if err != nil {
+		t.Errorf("error creating mock graphite: %v", err)
+	}
+	defer nmg.Close()
+
+	err = b.Push()
+	if err != nil {
+		t.Errorf("error pushing: %v", err)
+	}
+
+	wants := []string{
+		"prefix.nameconstname.constvalue.labelname.val1 1",
+		"prefix.nameconstname.constvalue.labelname.val2 1",
+	}
+
+	select {
+	case got := <-nmg.readc:
+		for _, want := range wants {
+			matched, err := regexp.MatchString(want, got)
+			if err != nil {
+				t.Errorf("error pushing: %v", err)
+			}
+			if !matched {
+				t.Errorf("missing metric:\nno match for %s received by server:\n%s", want, got)
+			}
+		}
+		fmt.Println(b)
+		return
+	case err := <-nmg.errc:
+		t.Errorf("error reading push: %v", err)
+	}
+
+	t.Fatal()
+}
+
+func newMockGraphite(port string) (*mockGraphite, error) {
+	readc := make(chan string)
+	errc := make(chan error)
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		conn, err := ln.Accept()
+		if err != nil {
+			errc <- err
+		}
+		var b bytes.Buffer
+		io.Copy(&b, conn)
+		readc <- b.String()
+	}()
+
+	return &mockGraphite{
+		readc:    readc,
+		errc:     errc,
+		Listener: ln,
+	}, nil
+}
+
+type mockGraphite struct {
+	readc chan string
+	errc  chan error
+
+	net.Listener
 }
