d703c70ef1e4de2a792fd786420eee6582626c5e
beorn7 <beorn@soundcloud.com>
2016-11-12 16:36:39 +0100

    Adjust examples to modern usage
    - Use seconds instead of microseconds.
    - Use promhttp.Handler instead of prometheus.Handler

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -19,13 +19,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
 func main() {
 	flag.Parse()
-	http.Handle("/metrics", prometheus.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
