efb2f142f3dcb52ef6fd03e5853011899648d9f3
Peng Gao <peng.gao.dut@gmail.com>
2017-02-15 13:06:22 +0800

    goCollector: change goroutines and treads type
    Change gorountines and threads created Gauge to NewConstMetric.
    
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- prometheus/go_collector.go
+++ prometheus/go_collector.go
@@ -8,9 +8,9 @@ import (
 )
 
 type goCollector struct {
-	goroutines Gauge
-	threads    Gauge
-	gcDesc     *Desc
+	goroutinesDesc *Desc
+	threadsDesc    *Desc
+	gcDesc         *Desc
 
 	// metrics to describe and collect
 	metrics memStatsMetrics
@@ -20,16 +20,14 @@ type goCollector struct {
 // go process.
 func NewGoCollector() Collector {
 	return &goCollector{
-		goroutines: NewGauge(GaugeOpts{
-			Namespace: "go",
-			Name:      "goroutines",
-			Help:      "Number of goroutines that currently exist.",
-		}),
-		threads: NewGauge(GaugeOpts{
-			Namespace: "go",
-			Name:      "threads",
-			Help:      "Number of threads created.",
-		}),
+		goroutinesDesc: NewDesc(
+			"go_goroutines",
+			"Number of goroutines that currently exist.",
+			nil, nil),
+		threadsDesc: NewDesc(
+			"go_threads",
+			"Number of OS threads created",
+			nil, nil),
 		gcDesc: NewDesc(
 			"go_gc_duration_seconds",
 			"A summary of the GC invocation durations.",
@@ -230,8 +228,8 @@ func memstatNamespace(s string) string {
 
 // Describe returns all descriptions of the collector.
 func (c *goCollector) Describe(ch chan<- *Desc) {
-	ch <- c.goroutines.Desc()
-	ch <- c.threads.Desc()
+	ch <- c.goroutinesDesc
+	ch <- c.threadsDesc
 	ch <- c.gcDesc
 	for _, i := range c.metrics {
 		ch <- i.desc
@@ -240,11 +238,9 @@ func (c *goCollector) Describe(ch chan<- *Desc) {
 
 // Collect returns the current state of all metrics of the collector.
 func (c *goCollector) Collect(ch chan<- Metric) {
-	c.goroutines.Set(float64(runtime.NumGoroutine()))
-	ch <- c.goroutines
+	ch <- MustNewConstMetric(c.goroutinesDesc, GaugeValue, float64(runtime.NumGoroutine()))
 	n, _ := runtime.ThreadCreateProfile(nil)
-	c.threads.Set(float64(n))
-	ch <- c.threads
+	ch <- MustNewConstMetric(c.threadsDesc, GaugeValue, float64(n))
 
 	var stats debug.GCStats
 	stats.PauseQuantiles = make([]time.Duration, 5)
