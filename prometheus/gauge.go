ceb819208d49dfac81eb35186fb44010eb85d4c5
Peter Jausovec <peter.jausovec@oracle.com>
2018-10-31 11:16:04 -0700

    copy/pasta error
    Signed-off-by: Peter Jausovec <peter.jausovec@oracle.com>

--- prometheus/gauge.go
+++ prometheus/gauge.go
@@ -148,7 +148,7 @@ func NewGaugeVec(opts GaugeOpts, labelNames []string) *GaugeVec {
 	return &GaugeVec{
 		metricVec: newMetricVec(desc, func(lvs ...string) Metric {
 			if len(lvs) != len(desc.variableLabels) {
-				err := fmt.Errorf("%s: labels and values for '%s' don't match: labels %v, values %v", errInconsistentCardinality, desc.fqName, desc.variableLabels, labelValues)
+				err := fmt.Errorf("%s: labels and values for '%s' don't match: labels %v, values %v", errInconsistentCardinality, desc.fqName, desc.variableLabels, lvs)
 				panic(err)
 			}
 			result := &gauge{desc: desc, labelPairs: makeLabelPairs(desc, lvs)}
