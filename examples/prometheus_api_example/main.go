23a2be6cd6a944234125dcc61ce5e59bae1f99f4
Prakash Mishra <prakashmishra1598@gmail.com>
2019-01-31 20:13:48 +0530

    RUN go fmt on main.go
    Signed-off-by: Prakash Mishra <prakashmishra1598@gmail.com>

--- examples/prometheus_api_example/main.go
+++ examples/prometheus_api_example/main.go
@@ -18,13 +18,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/prometheus/client_golang/api/prometheus/v1"
 	prometheus "github.com/prometheus/client_golang/api"
+	"github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
 	"time"
 )
 
-func main()  {
+func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -33,8 +33,7 @@ func main()  {
 
 	fmt.Println("Getting container memory usage...")
 
-
-	config := prometheus.Config{Address: hostname, RoundTripper:prometheus.DefaultRoundTripper}
+	config := prometheus.Config{Address: hostname, RoundTripper: prometheus.DefaultRoundTripper}
 	client, err := prometheus.NewClient(config)
 	if err != nil {
 		fmt.Println(err)
@@ -42,7 +41,7 @@ func main()  {
 	httpApi := v1.NewAPI(client)
 
 	//Query time series data(memory usage in bytes for containers) for 5m
-	queryString := "container_memory_usage_bytes{pod_name=\"" + podName +"\"}[5m]"
+	queryString := "container_memory_usage_bytes{pod_name=\"" + podName + "\"}[5m]"
 	//Replace above query_string with your own query
 
 	result, err := httpApi.Query(ctx, queryString, time.Now()) //returns a model.Value interface (defined in (https://github.com/prometheus/common/blob/master/model/value.go)
@@ -51,13 +50,12 @@ func main()  {
 		fmt.Println(err)
 	}
 
-
 	numberOfContainersInPod := result.(model.Matrix).Len()
 	fmt.Println("Number of containers in pod = ", numberOfContainersInPod)
 
 	for i, s := range result.(model.Matrix) { //cast model.Value to model.Matrix which is an array of SamplePair structs (defined in (https://github.com/prometheus/common/blob/master/model/value.go)
-		for _, k  := range s.Values {
-			fmt.Println("At(timestamp)=" + k.Timestamp.String() + ", memory usage(in bytes) for container #", i, "=" + k.Value.String())
+		for _, k := range s.Values {
+			fmt.Println("At(timestamp)="+k.Timestamp.String()+", memory usage(in bytes) for container #", i, "="+k.Value.String())
 		}
 	}
 }
