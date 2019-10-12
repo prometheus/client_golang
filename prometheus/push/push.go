7c6a9d7568521fdbf1aa0d4e6e7b6108d477ce70
Xin Zhang <ahjdzx1990@gmail.com>
2019-10-12 16:07:00 +0800

    Fix check status code
    Signed-off-by: Xin Zhang <ahjdzx1990@gmail.com>

--- prometheus/push/push.go
+++ prometheus/push/push.go
@@ -267,7 +267,7 @@ func (p *Pusher) push(method string) error {
 		return err
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode != 202 {
+	if resp.StatusCode != 200 && resp.StatusCode != 202 {
 		body, _ := ioutil.ReadAll(resp.Body) // Ignore any further error as this is for an error message only.
 		return fmt.Errorf("unexpected status code %d while pushing to %s: %s", resp.StatusCode, p.fullURL(), body)
 	}
