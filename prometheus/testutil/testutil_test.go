b33c84859be2665513eca0a3b46ed4dca1a34bae
Sourik Ghosh <sourikghosh31@gmail.com>
2022-03-17 13:00:15 +0530

    replaced CollectAndCompare to CollectAndCompareV2 in testutil_test
    Signed-off-by: Sourik Ghosh <sourikghosh31@gmail.com>

--- prometheus/testutil/testutil_test.go
+++ prometheus/testutil/testutil_test.go
@@ -139,7 +139,7 @@ func TestCollectAndCompare(t *testing.T) {
 		some_total{ label1 = "value1" } 1
 	`
 
-	if err := CollectAndCompare(t, c, strings.NewReader(metadata+expected), false, "some_total"); err != nil {
+	if err := CollectAndCompareV2(t, c, strings.NewReader(metadata+expected), false, "some_total"); err != nil {
 		t.Errorf("unexpected collecting result:\n%s", err)
 	}
 }
@@ -161,7 +161,7 @@ func TestCollectAndCompareNoLabel(t *testing.T) {
 		some_total 1
 	`
 
-	if err := CollectAndCompare(t, c, strings.NewReader(metadata+expected), false, "some_total"); err != nil {
+	if err := CollectAndCompareV2(t, c, strings.NewReader(metadata+expected), false, "some_total"); err != nil {
 		t.Errorf("unexpected collecting result:\n%s", err)
 	}
 }
@@ -233,7 +233,7 @@ func TestCollectAndCompareHistogram(t *testing.T) {
 		}
 
 		t.Run(input.name, func(t *testing.T) {
-			if err := CollectAndCompare(t, input.c, strings.NewReader(input.metadata+input.expect), false); err != nil {
+			if err := CollectAndCompareV2(t, input.c, strings.NewReader(input.metadata+input.expect), false); err != nil {
 				t.Errorf("unexpected collecting result:\n%s", err)
 			}
 		})
@@ -260,7 +260,7 @@ func TestNoMetricFilter(t *testing.T) {
 		some_total{label1="value1"} 1
 	`
 
-	if err := CollectAndCompare(t, c, strings.NewReader(metadata+expected), false); err != nil {
+	if err := CollectAndCompareV2(t, c, strings.NewReader(metadata+expected), false); err != nil {
 		t.Errorf("unexpected collecting result:\n%s", err)
 	}
 }
@@ -298,7 +298,7 @@ got:
 some_total{label1="value1"} 1
 `
 
-	err := CollectAndCompare(t, c, strings.NewReader(metadata+expected), true)
+	err := CollectAndCompareV2(t, c, strings.NewReader(metadata+expected), true)
 	if err == nil {
 		t.Error("Expected error, got no error.")
 	}
