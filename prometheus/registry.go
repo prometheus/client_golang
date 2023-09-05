7d536023aad068199d5c1c74f0eb74f0937302e5
Owen Williams <owen.williams@grafana.com>
2023-09-05 10:30:18 -0400

    bit of cleanup

--- prometheus/registry.go
+++ prometheus/registry.go
@@ -683,13 +683,7 @@ func processMetric(
 		}
 	} else { // New name.
 		metricFamily = &dto.MetricFamily{}
-		fqName := desc.fqName
-		debug.PrintStack()
-		// if !model.IsValidMetricName(model.LabelValue(fqName)) {
-		// 	fqName = fmt.Sprintf(`"%s"`, fqName)
-		// 	fmt.Printf("adding quotes I hope: %s\n", fqName)
-		// }
-		metricFamily.Name = proto.String(fqName)
+		metricFamily.Name = proto.String(desc.fqName)
 		metricFamily.Help = proto.String(desc.help)
 		// TODO(beorn7): Simplify switch once Desc has type.
 		switch {
