504566f07c680f68743c3a5d239dede48538c7ec
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2024-09-02 14:58:23 +0200

    Use simplified solution from #1609 for the data race
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- prometheus/histogram.go
+++ prometheus/histogram.go
@@ -1673,6 +1673,7 @@ func makeNativeExemplars(ttl time.Duration, maxCount int) nativeExemplars {
 
 	if maxCount < 0 {
 		maxCount = 0
+		ttl = -1
 	}
 
 	return nativeExemplars{
@@ -1682,12 +1683,12 @@ func makeNativeExemplars(ttl time.Duration, maxCount int) nativeExemplars {
 }
 
 func (n *nativeExemplars) addExemplar(e *dto.Exemplar) {
-	n.Lock()
-	defer n.Unlock()
-
-	if cap(n.exemplars) == 0 {
+	if n.ttl == -1 {
 		return
 	}
+
+	n.Lock()
+	defer n.Unlock()
 
 	// The index where to insert the new exemplar.
 	var nIdx int = -1
