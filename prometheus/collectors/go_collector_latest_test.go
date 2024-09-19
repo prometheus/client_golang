154e9da4cafb001290980983bfc94a0ce1cf8231
Arianna Vespri <arianna.vespri@yahoo.it>
2024-09-19 16:52:06 +0200

    Adapt withDefaultRuntimeMetrics function to work regardless of the Go version
    Signed-off-by: Arianna Vespri <arianna.vespri@yahoo.it>

--- prometheus/collectors/go_collector_latest_test.go
+++ prometheus/collectors/go_collector_latest_test.go
@@ -66,14 +66,17 @@ func withDefaultRuntimeMetrics(metricNames []string, withoutGC, withoutSched boo
 	if withoutGC && withoutSched {
 		// If both flags are true, return the metricNames as is.
 		return metricNames
-	} else if withoutGC && !withoutSched {
-		// If only withoutGC is true, include "go_sched_gomaxprocs_threads" only.
+	} else if withoutGC && !withoutSched && defaultRuntimeMetrics != nil {
+		// If only withoutGC is true and the Go version is at least 1.20, include "go_sched_gomaxprocs_threads" only.
 		metricNames = append(metricNames, []string{"go_sched_gomaxprocs_threads"}...)
-	} else if withoutSched && !withoutGC {
-		// If only withoutSched is true, exclude "go_sched_gomaxprocs_threads".
+	} else if withoutSched && !withoutGC && len(defaultRuntimeMetrics) > 1 {
+		// If only withoutSched is true and the Go version is higher than 1.20, exclude "go_sched_gomaxprocs_threads".
 		metricNames = append(metricNames, []string{"go_gc_gogc_percent", "go_gc_gomemlimit_bytes"}...)
+	} else if withoutSched && len(defaultRuntimeMetrics) == 1 {
+		// If only withoutSched is true and the Go version is 1.20, return the metricNames as is.
+		return metricNames
 	} else {
-		// If neither flag is true, use the default metrics.
+		// In any other case, use the default metrics.
 		metricNames = append(metricNames, defaultRuntimeMetrics...)
 	}
 	// sorting is required
