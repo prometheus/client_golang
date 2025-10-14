343ae65ca0b845a576c75c5fcd6e8e55ba451358
Minh Nguyen <148210689+pipiland2612@users.noreply.github.com>
2025-10-14 13:53:53 +0300

    fix bug (#1892)
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>

--- prometheus/go_collector_metrics_go124_test.go
+++ prometheus/go_collector_metrics_go124_test.go
@@ -52,6 +52,7 @@ var (
 		"/godebug/non-default-behavior/gotypesalias:events":                "go_godebug_non_default_behavior_gotypesalias_events_total",
 		"/godebug/non-default-behavior/http2client:events":                 "go_godebug_non_default_behavior_http2client_events_total",
 		"/godebug/non-default-behavior/http2server:events":                 "go_godebug_non_default_behavior_http2server_events_total",
+		"/godebug/non-default-behavior/httpcookiemaxnum:events":            "go_godebug_non_default_behavior_httpcookiemaxnum_events_total",
 		"/godebug/non-default-behavior/httplaxcontentlength:events":        "go_godebug_non_default_behavior_httplaxcontentlength_events_total",
 		"/godebug/non-default-behavior/httpmuxgo121:events":                "go_godebug_non_default_behavior_httpmuxgo121_events_total",
 		"/godebug/non-default-behavior/httpservecontentkeepheaders:events": "go_godebug_non_default_behavior_httpservecontentkeepheaders_events_total",
@@ -109,4 +110,4 @@ var (
 	}
 )
 
-const expectedRuntimeMetricsCardinality = 172
+const expectedRuntimeMetricsCardinality = 173
